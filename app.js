92535ecf1027c7ec8d799c52d4e8df3d1b49f997
unknown <you@example.com>
2024-08-10 02:24:02 +0200

    cors

--- app.js
+++ app.js
@@ -24,17 +24,12 @@ app.use(session({
 }));
 console.log(process.env.jwtPrivateKey)
 app.use(helmet());
-app.use(cors({
-    origin: function (origin, callback) {
-        // Permitir solicitudes sin origen (como las de herramientas locales o pruebas de postman)
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.indexOf(origin) === -1) {
-            const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-            return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-    }
-}));
+app.use(
+  cors({
+    credentials: true,
+    origin:"https://proyect-final-front.pages.dev",
+  })
+);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
